c4ab5108cd022d1fcab2f00ab8686d97df5006de
KittyDuckBot <hongkhuyen860@gmail.com>
2025-03-23 15:18:55 +0100

    Task12

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+#1
 name = input("your name: ")
 age = input("your age: ")
 print(f"Hello {name}, you are {age}!")
+
+#2
+age = int(input("your age: "))
+if age >= 18:
+    print("entry allowed")
+else:
+    print("entry blocked")
